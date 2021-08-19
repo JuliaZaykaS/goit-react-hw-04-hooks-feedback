361d0ba7434b2d13acf88aa4d3b1e032106e88d7
JuliaZaykaS <sys55@mail.ru>
2021-08-19 14:55:33 +0300

    перевела на хуки

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -11,32 +11,33 @@ export default function App() {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  useEffect(() => {
-    const countTotalFeedback = () => {
+  // useEffect(() => {
+  //   const countTotalFeedback = () => {
 
-      const total = good + neutral + bad;
-      return total;
-    };
-    const countPositiveFeedbackPercentage = () => {
-      if (good === 0) return 0;
-      const positiveFeedback = `${Math.round(
-        (good / this.countTotalFeedback()) * 100,
-      )}%`
-      return positiveFeedback;
-    }
+  //     const total = good + neutral + bad;
+  //     return total;
+  //   };
+  //   const countPositiveFeedbackPercentage = () => {
+  //     if (good === 0) return 0;
+  //     const positiveFeedback = `${Math.round(
+  //       (good / countTotalFeedback()) * 100,
+  //     )}%`
+  //     return positiveFeedback;
+  //   }
 
-    countTotalFeedback();
-    countPositiveFeedbackPercentage();
-  }, [good, neutral, bad]);
+  //   // countTotalFeedback();
+  //   // countPositiveFeedbackPercentage();
+  // }, [good, neutral, bad]);
 
   const countTotalFeedback = () => {
     const total = good + neutral + bad;
     return total;
   };
+
   const countPositiveFeedbackPercentage = () => {
     if (good === 0) return 0;
     const positiveFeedback = `${Math.round(
-      (good / this.countTotalFeedback()) * 100,
+      (good / countTotalFeedback()) * 100,
     )}%`
     return positiveFeedback;
   };
@@ -139,5 +140,72 @@ return (
 );
 //   }
 }
+
+// export default App;
+
+// import { Component } from 'react';
+// import Section from '../Section/Section';
+// import FeedbackOptions from '../FeedbackOptions/FeedbackOptions';
+// import Statistics from '../Statistics/Statistics';
+// import Notification from '../Notification/Notification';
+// import s from './App.module.css';
+
+// class App extends Component {
+//   state = {
+//     good: 0,
+//     neutral: 0,
+//     bad: 0,
+//   };
+
+//   onClickBtn = option => {
+//     return this.setState(prevState => ({
+//       [option.toLowerCase()]: prevState[option.toLowerCase()] + 1,
+//       // [option]: prevState[option] + 1,
+//     }));
+//   };
+
+//   countTotalFeedback() {
+//     const { good, neutral, bad } = this.state;
+//     const total = good + neutral + bad;
+//     return total;
+//   }
+//   countPositiveFeedbackPercentage() {
+//     const { good } = this.state;
+//     if (good === 0) return 0;
+//     const positiveFeedback = `${Math.round(
+//       (good / this.countTotalFeedback()) * 100,
+//     )}%`
+//     return positiveFeedback;
+//   }
+
+//   render() {
+//     const { good, neutral, bad } = this.state;
+//     const total = this.countTotalFeedback();
+//     const options = ['Good', 'Neutral', 'Bad'];
+//     return (
+//       <div className={s.app}>
+//         <Section title={'Please leave feedback'}>
+//           <FeedbackOptions
+//             options={options}
+//             onLeaveFeedback={this.onClickBtn}
+//           />
+//         </Section>
+//         <Section title={'Statistics'}>
+//           {total === 0 ? (
+//             <Notification message={'No feedback given'} />
+//           ) : (
+//             <Statistics
+//               good={good}
+//               neutral={neutral}
+//               bad={bad}
+//               total={total}
+//               positiveFeedback={this.countPositiveFeedbackPercentage()}
+//             />
+//           )}
+//         </Section>
+//       </div>
+//     );
+//   }
+// }
 
 // export default App;
